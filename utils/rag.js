6eca330ebd50f4dce776567178a9b708920f90fc
Parth Singh <parthsingh503@gmail.com>
2025-10-16 21:34:04 +0530

    Loading doc completed

--- utils/rag.js
+++ utils/rag.js
@@ -4,6 +4,6 @@ import { indexTheDocument } from "@/utils/prepare";
 export const testRag = async () => {
   const filePath = "public/yash.pdf";
   const docs = await indexTheDocument(filePath);
-  console.log(docs);
+  console.log(docs[0].pageContent);
   return docs;
 };
